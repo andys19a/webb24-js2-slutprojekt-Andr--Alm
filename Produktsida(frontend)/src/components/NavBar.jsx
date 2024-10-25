1d58dd31a2142ed65343feb521ca98283438bcd4
andys19a <159772694+andys19a@users.noreply.github.com>
2024-10-25 15:25:56 +0200

    Update NavBar.jsx

--- Produktsida(frontend)/src/components/NavBar.jsx
+++ Produktsida(frontend)/src/components/NavBar.jsx
@@ -9,7 +9,6 @@ export function NavBar({ showProducts, showCart, cartItemCount, loading, product
           Kundvagn {cartItemCount > 0 && <span>({cartItemCount})</span>}
         </li>
       </ul>
-// Ligger här istället för app pga navbar ska visas om backend är nere / produkter inte visas
       {/* Lägg till logik för att hantera laddning och tomma produkter */}
       {loading ? (
         <div>Laddar produkter...</div>
